54255b2764de6e59b1ea29c377ffe6de17620a37
caio_ingber <caioingber@gmail.com>
2020-02-06 18:20:50 -0500

    resolved modal issues, showing article and description

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //locations = [london, durban, DC, Rio, Tokyo]
 // key = eebcb3a9d0e7471c851bb8dbaa15fad4
-let locations = [44418, 1580913, 2514815, 455825, 1118370]
 let boxes = document.querySelectorAll('.news-box')
 let title = document.querySelectorAll('.headline-text')
 let date = document.querySelectorAll('.publish-date')
+let hidden = document.querySelector('.modal')
+
+let body = document.querySelector('main')
+
 
 
 let nameUrl = "https://www.metaweather.com/api/location/search/?query=london"
@@ -20,17 +23,32 @@ fetch (baseUrl)
         console.log(res)
         for (let i=0; i < 6; i++) {
         //fetch image for article
-        let image = res.articles[i].urlToImage
-        boxes[i].style.backgroundImage = `url(${image})`
-        boxes[i].style.backgroundSize = 'contain'
-        boxes[i].style.backgroundPosition = 'center bottom'
-        boxes[i].style.backgroundRepeat = 'no-repeat'
-        //fetch headline
-        let headline = res.articles[i].title
-        title[i].innerText = headline
-        //fetch timestamp
-        let publishTime = res.articles[i].publishedAt
-        let trimStamp = publishTime.substring(0, 10)
-        date[i].innerHTML = trimStamp
+            let image = res.articles[i].urlToImage
+            boxes[i].style.backgroundImage = `url(${image})`
+            boxes[i].style.backgroundSize = 'contain'
+            boxes[i].style.backgroundPosition = 'center bottom'
+            boxes[i].style.backgroundRepeat = 'no-repeat'
+            //fetch headline
+            let headline = res.articles[i].title
+            title[i].innerText = headline
+            //fetch timestamp
+            let publishTime = res.articles[i].publishedAt
+            let trimStamp = publishTime.substring(0, 10)
+            date[i].innerHTML = trimStamp
+            
+            //eventlistener Modal
+            function showModal() {
+                // e.preventDefault()
+                hidden.style.opacity = 1
+                hidden.style.zIndex = 1
+                let modalHeadline = document.querySelector('.headline-modal')
+                modalHeadline.innerHTML = headline
+                let modalDescription = document.querySelector('.news-description')
+                modalDescription.innerHTML = res.articles[i].description
+            }
+            boxes[i].addEventListener('click', showModal)
+
     }
     })
+
+ 
