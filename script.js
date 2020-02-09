cd2b99522c4af79ce4a5102f173069350eb5b0aa
caio_ingber <caioingber@gmail.com>
2020-02-09 15:32:44 -0500

    added logo to header

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let title = document.querySelectorAll('.headline-text')
 let timeStamp = document.querySelector('.publish-date')
 let modContainer = document.querySelector('.modal-container')
 let modal = document.querySelector('.modal')
-let images = document.querySelectorAll('img')
+let images = document.querySelectorAll('.stretch-img')
 let xButton = document.querySelector('#close-text')
 let contentBox = document.querySelector('#news-content')
 
@@ -72,11 +72,11 @@ fetch (baseUrl)
                 modalImage.style.backgroundImage = `url(${image})`
                 //show article content modal
                 let artContent = res.articles[i].content.substring(0, 220)
-                //removing line breaks from API
-                // let contentArr = artContent.split('\r\n')
-                // contentArr = contentArr.concat()
-                // let modContent = contentArr.toString()
-                contentBox.innerText = `${artContent}...`
+                // removing line breaks from API
+                let contentArr = artContent.split('\r\n')
+                contentArr = contentArr.join(". ")
+                let modContent = contentArr.toString()
+                contentBox.innerText = `${modContent}...`
                 //Show publish date in modal
                 let publishTime = res.articles[i].publishedAt
                 let trimStamp = publishTime.substring(0, 10)
